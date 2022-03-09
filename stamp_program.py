e4db9691aedbabbfe848477a7c5e5f982ea3baba
Isaac Telloian <iketelloian@fresnostate.edu>
2022-03-08 17:56:43 -0800

    first commit

---
+++ stamp_program.py
@@ -0,0 +1,19 @@
+"""
+start
+get the numbers of sheets
+sheet / 5
+round answer to next number
+output the results to the user
+end
+"""
+def calculate(sheet):
+    answer = sheet / 5
+    rounded = round(answer, 1)
+    print("sheet is: ", sheet)
+    print("The answer is: ", answer)
+    print("rounded is: ", rounded)
+    return rounded # this return is 
+    
+output = calculate (696969696969696)
+
+print("the return statement is: ", output)
